7569e37ebac49b419e9ca8a82ae8a5725f87be38
Mauro Ciancio <mauro.ciancio@cymait.com>
2012-10-18 00:01:14 -0300

    ejemplo de transaccionalidad

--- src/grails-app/services/backlogend/UserService.groovy
+++ src/grails-app/services/backlogend/UserService.groovy
@@ -4,12 +4,21 @@ class UserService {
 	static transactional = true // por default es true, esta linea es opcional
 	
     def createNewUser(String username, String password) {
+		log.error("createNewUser() invoked")
+
+		log.error("creando usuario")
 		// creamos un usuario y lo grabamos
 		def user = new User(name: username, password: password)
-		user.save()
+		user.save(insert: true)
+		log.error("usuario insertado")
 
+		// descomentar para probar la transaccionalidad
+		// throw new IllegalArgumentException();
+
+		log.error("creando story theme")
 		// creamos un story theme
 		def userTheme = new StoryTheme(description: "El usuario ${username} creó este story theme.")
-		userTheme.save(failOnError: true)
+		userTheme.save()
+		log.error("story theme creado")
     }
 }
